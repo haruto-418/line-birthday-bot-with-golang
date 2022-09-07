49f296d2765c173624bbfa27af55a58239374b2a
KikuchiHaruto <o5o206felt@icloud.com>
2022-09-07 22:14:28 +0900

    fix: main.go testData

--- main.go
+++ main.go
@@ -3,24 +3,40 @@ package main
 import (
 	"log"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/line/line-bot-sdk-go/linebot"
 )
 
+const testData="2000-09-07"
+
+
 func main(){
-	if err:=godotenv.Load(".env"); err!=nil{
-		log.Fatal(err)
+	location,_:=time.LoadLocation("Asia/Tokyo")
+	var is_celebrating bool = false
+	month:=time.Now().In(location).Month()
+	day:=time.Now().In(location).Day()
+	target,_:=time.ParseInLocation("2006-01-02",testData,location)
+	if month==target.Month() && day==target.Day(){
+		is_celebrating=true
 	}
-	bot,err:=linebot.New(
-		os.Getenv("LINE_BOT_CHANNEL_SECRET"),
-		os.Getenv("LINE_BOT_CHANNEL_TOKEN"),
-	)
-	if err!=nil{
-		log.Fatal(err)
-	}
-	message:=linebot.NewTextMessage("hello")
-	if _,err:=bot.BroadcastMessage(message).Do(); err!=nil{
-		log.Fatal(err)
+	if is_celebrating{
+		// 一致する場合はline bot を起動。
+		if err:=godotenv.Load(".env"); err!=nil{
+			log.Fatal(err)
+		}
+		bot,err:=linebot.New(
+			os.Getenv("LINE_BOT_CHANNEL_SECRET"),
+			os.Getenv("LINE_BOT_CHANNEL_TOKEN"),
+		)
+		if err!=nil{
+			log.Fatal(err)
+		}
+		message:=linebot.NewTextMessage("hello")
+		if _,err:=bot.BroadcastMessage(message).Do(); err!=nil{
+			log.Fatal(err)
+		}
 	}
 }
+

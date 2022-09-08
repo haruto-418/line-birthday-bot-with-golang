0c9836589a5132058e5c7e9189800f1b6e02e5fb
KikuchiHaruto <o5o206felt@icloud.com>
2022-09-08 21:40:39 +0900

    delete: user_controller.go, fix: time.go

--- utils/time.go
+++ utils/time.go
@@ -1,21 +1,2 @@
 package utils
 
-import (
-	"log"
-	"time"
-)
-
-func IsCelebrating(target string)bool{
-	location,_:=time.LoadLocation("Asia/Tokyo")
-	month:=time.Now().In(location).Month()
-	day:=time.Now().In(location).Day()
-	target_time,err:=time.ParseInLocation("2006-01-02",target,location)
-	if err!=nil{
-		log.Fatal(err)
-	}
-	var is_celebrating bool = false
-	if month==target_time.Month() && day==target_time.Day(){
-		is_celebrating=true
-	}
-	return is_celebrating
-}
